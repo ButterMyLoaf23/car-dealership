bad5d25ac63f6c3da25a32eee83394f6f18d3d42
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-03-23 18:13:23 -0700

    made some changes to db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,21 @@
-import pkg from "pg";
+import { pool } from "pg";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { checkServerIdentity } from "tls";
 
-const { Pool } = pkg;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const caCert = fs.readFileSync(path.join(__dirname, "../bin/", "byuicse-psql-cert.pem"));
+
 
 const pool = new Pool ({
     connectionString: process.env.DB_URL,
     ssl: {
-        rejectUnauthorized: false
+        ca: caCert,
+        rejectUnauthorized: true,
+        checkServerIdentity: () => { return undefined; }
     }
 });
 
