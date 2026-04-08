2fd9775aacd90bbc3603f46adfd03ea075129c26
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-04-08 12:47:41 -0700

    Made a lot of image url changes, changed css

--- src/models/vehicles.js
+++ src/models/vehicles.js
@@ -1,7 +1,7 @@
 import pool from "./db.js";
 // this is my featured vehicles for my homepage
 export const getFeaturedVehicles = async () => {
-    const result = await pool.query ("SELECT * FROM vehicles ORDER BY id DESC LIMIT 6");
+    const result = await pool.query ("SELECT * FROM vehicles ORDER BY id DESC LIMIT 3");
     return result.rows;
 }
 
